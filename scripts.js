0b2372c8c777b01b862b1b587957f4a0d48f107e
Daniel <daniel_mn_casey@hotmail.com>
2025-02-17 11:20:32 +0000

    fixed sizing and removed button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 function button_clicked(){
     var e = document.getElementById("imageholder");
-    e.innerHTML = '<img id="image" src="baby.jpg" alt="no-image" style="height:200px, width:100px">'
+    e.innerHTML = '<img id="image" src="baby.jpg" alt="no-image" style=" height: 500px; width: 500px; display: block;margin-left: auto;margin-right: auto;width: 50%;">'
     var l = document.getElementById("label");
     l.innerText = "Baby Casey due on the 25th of august!!!!"
+    document.getElementById("button").remove();
 }
