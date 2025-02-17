3c5a58dbb551a363069330bba223143cc5c91dbd
Daniel <daniel_mn_casey@hotmail.com>
2025-02-17 10:54:47 +0000

    changed size of image

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function button_clicked(){
-    var e = document.getElementById("image");
-    e.src = "baby.jpg"
+    var e = document.getElementById("imageholder");
+    e.innerHTML = '<img id="image" src="baby.jpeg" alt="no-image" style="height=500px, width=250px">'
     var l = document.getElementById("label");
     l.innerText = "Baby Casey due on the 25th of august!!!!"
 }
