551d82081168af0b73b6115981d550de1330686b
Daniel <daniel_mn_casey@hotmail.com>
2025-02-17 11:28:33 +0000

    fixed formatting and made button bigger

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@ function button_clicked(){
     var e = document.getElementById("imageholder");
     e.innerHTML = '<img id="image" src="baby.jpg" alt="no-image" style=" height: 500px; width: 500px; display: block;margin-left: auto;margin-right: auto;width: 50%;">'
     var l = document.getElementById("label");
-    l.innerText = "Baby Casey due on the 25th of august!!!!"
+    l.innerText = "Baby Casey due on the 25th of August!!!!"
     document.getElementById("button").remove();
 }
