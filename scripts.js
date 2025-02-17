d4e6e7c2d5b99438a573ac78dfa9cba1264f9989
Daniel <daniel_mn_casey@hotmail.com>
2025-02-17 10:36:38 +0000

    initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function button_clicked(){
+    var e = document.getElementById("image");
+    e.src = "baby.jpg"
+    var l = document.getElementById("label");
+    l.innerText = "Baby Casey due on the 25th of august!!!!"
+}
