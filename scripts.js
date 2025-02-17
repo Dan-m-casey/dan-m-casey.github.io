88a1a61b035f0d0804a32be8c546b58af756b6e1
Daniel <daniel_mn_casey@hotmail.com>
2025-02-17 11:03:22 +0000

    tried to fix the css

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function button_clicked(){
     var e = document.getElementById("imageholder");
-    e.innerHTML = '<img id="image" src="baby.jpg" alt="no-image" style="height=200px, width=100px">'
+    e.innerHTML = '<img id="image" src="baby.jpg" alt="no-image" style="height:200px, width:100px">'
     var l = document.getElementById("label");
     l.innerText = "Baby Casey due on the 25th of august!!!!"
 }
